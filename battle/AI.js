f7772f66789a8d4f7d90c74f80299c746fc07c1b
momiiyuya <u_sunset@ezweb.ne.jp>
2017-10-15 16:19:45 +0900

    AIが相手との距離を考慮

--- battle/AI.js
+++ battle/AI.js
@@ -45,10 +45,6 @@ function AI_1(){//最大火力のマスを選択する簡単なAI
 		// tSkill=mSkillList[tSkill];
 		let tRange=calcRange(tSkill.RANGE,{x:mMovable[i][0],y:mMovable[i][1]});
 		if(tCard[1]!="joker"&&tCard[1]!="suka"){//jokerとsukaは攻撃範囲を表示しない
-			if(tChara.MP<tSkill.MAGIC){
-				tPrioritys.push(tPriority);
-				continue;
-			}
 			var tDamegeCharas = new Array();
 			if(mDelayChara[1]=="T"){
 				tDamegeCharas = mFalseTeam;
@@ -56,16 +52,18 @@ function AI_1(){//最大火力のマスを選択する簡単なAI
 			else{
 				tDamegeCharas = mTrueTeam;
 			}
-			for(let i=0;i<tRange.length;i++){
-				for(var j=0;j<tDamegeCharas.length;j++){
-					if(tRange[i][0]==tDamegeCharas[j].x&&tRange[i][1]==tDamegeCharas[j].y){
-						var tDamage = calcDamage(tChara.ATK,tDamegeCharas[j].DEF,tSkill.POWER);
+			if(tChara.MP>tSkill.MAGIC){
+				for(let i=0;i<tRange.length;i++){
+					for(var j=0;j<tDamegeCharas.length;j++){
+						if(tRange[i][0]==tDamegeCharas[j].x&&tRange[i][1]==tDamegeCharas[j].y){
+							var tDamage = calcDamage(tChara.ATK,tDamegeCharas[j].DEF,tSkill.POWER);
 
-						if(tCard[1]==tChara.TYPE){//属性補正
-							tDamage = Math.floor(tDamage*1.5);
+							if(tCard[1]==tChara.TYPE){//属性補正
+								tDamage = Math.floor(tDamage*1.5);
+							}
+
+							tPriority+=tDamage;
 						}
-
-						tPriority+=tDamage;
 					}
 				}
 			}
@@ -75,7 +73,12 @@ function AI_1(){//最大火力のマスを選択する簡単なAI
 				if(tCard[1]==tChara.TYPE){//属性補正
 					tDamage = Math.floor(tDamage*1.5);
 				}
-				tPriority-=tDamage
+				tPriority-=tDamage;
+			}
+			tPriority*=100;
+			for(var m=0;m<tDamegeCharas.length;m++){
+				tPriority -= Math.abs(tChara.x-tDamegeCharas[m].x);
+				tPriority -= Math.abs(tChara.y-tDamegeCharas[m].y);
 			}
 		}
 		// console.log(tCard[0]+1,tCard[1],tPriority);
