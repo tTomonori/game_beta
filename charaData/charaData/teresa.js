5316f36b11b829a0fcf20c5bf9d2b51611d30328
momiiyuya <u_sunset@ezweb.ne.jp>
2017-11-03 20:50:01 +0900

    テレサ追加

--- charaData/charaData/teresa.js
+++ charaData/charaData/teresa.js
@@ -110,17 +110,17 @@ class teresa extends Chara{
 				ANIMATION:[0]
 			},
 			{NUMBER:106,
-				TEXT:"十字に威力２ダメージ",
-				RANGE:[["vertical",1],["horizontal",1]],
-				POWER:2,
+				TEXT:"自分に威力２の回復をしてDELAYを60下げる (タイプ一致時80)",
+				RANGE:[],
+				POWER:0,
 				DELAY:0,
-				MAGIC:0,
+				MAGIC:5,
 				SUPPORT_Be_Myself:[],
-				SUPPORT_Af_Myself:[],
+				SUPPORT_Af_Myself:[{effect:"delay",value:-60,additionOfMatchingType:-20}],
 				SUPPORT_Be_Enemy:[],
 				SUPPORT_Af_Enemy:[],
 				SUPPORT_Otherwise:[],
-				M_ATTACK:0,
+				M_ATTACK:-2,
 				F_ATTACK:false,
 		    E_ATTACK:true,
 				ANIMATION:[0]
@@ -142,13 +142,13 @@ class teresa extends Chara{
 				ANIMATION:[0]
 			},
 			{NUMBER:108,
-				TEXT:"相手全体に威力２のダメージ",
+				TEXT:"相手全体に威力２の回復 その後MPを8回復(タイプ一致時10)",
 				RANGE:[["enemy"]],
-				POWER:2,
-				DELAY:10,
+				POWER:-2,
+				DELAY:0,
 				MAGIC:0,
 				SUPPORT_Be_Myself:[],
-				SUPPORT_Af_Myself:[],
+				SUPPORT_Af_Myself:[{effect:"mp",value:8,additionOfMatchingType:2}],
 				SUPPORT_Be_Enemy:[],
 				SUPPORT_Af_Enemy:[],
 				SUPPORT_Otherwise:[],
@@ -158,11 +158,11 @@ class teresa extends Chara{
 				ANIMATION:[5]
 			},
 			{NUMBER:109,
-				TEXT:"周囲８マスに威力３のダメージ",
-				RANGE:[["square",3]],
-				POWER:3,
+				TEXT:"キャラクター全体に敵味方無視の威力３の回復",
+				RANGE:[["ally"],["enemy"]],
+				POWER:-3,
 				DELAY:0,
-				MAGIC:0,
+				MAGIC:2,
 				SUPPORT_Be_Myself:[],
 				SUPPORT_Af_Myself:[],
 				SUPPORT_Be_Enemy:[],
@@ -171,7 +171,7 @@ class teresa extends Chara{
 				M_ATTACK:0,
 				F_ATTACK:false,
 		    E_ATTACK:true,
-				ANIMATION:[3]
+				ANIMATION:[7]
 			},
 			{NUMBER:110,
 				TEXT:"ランダムな８マスに味方が踏むと２０回復する地雷を設置",
@@ -180,7 +180,7 @@ class teresa extends Chara{
 				DELAY:0,
 				MAGIC:2,
 				SUPPORT_Be_Myself:[],
-				SUPPORT_Af_Myself:[{effect:"setTrap",trapEffect:"mine",value:-20,target:["enemy"],remove:true,range:[["random",10]]}],
+				SUPPORT_Af_Myself:[{effect:"setTrap",trapEffect:"mine",value:-20,target:["ally"],remove:true,range:[["random",8]]}],
 				SUPPORT_Be_Enemy:[],
 				SUPPORT_Af_Enemy:[],
 				SUPPORT_Otherwise:[],
@@ -272,12 +272,300 @@ class teresa extends Chara{
 	}	//ターン開始時
 	startTurn(){
 		return new Promise((res,rej)=>{
+
 			//mp回復
 			this.useMp(-1);
 			//getTurnFlag
 			this.additionalTurnFlag=this.getTurnFlag;
 			this.getTurnFlag=false;
+
+			console.log(this)
+			console.log(this.image,1098010001)
+			console.log(this.HP,this.originalHP)
+			console.log(this.IMAGE==1098010001)
+			console.log(this.HP==this.originalHP)
+			if(this.image==1098010001&&this.HP==this.originalHP){
+				console.log("in");
+				attackAnimate(this,this,[7],()=>{
+					freeLog(this,"服装","変化した");
+				this.transform(this.getTransformData(1));
+				res()})
+			}
+			else if(this.image==1098020001&&this.HP!=this.originalHP){
+				console.log("in2");
+				attackAnimate(this,this,[7],()=>{
+					freeLog(this,"服装","元に戻った");
+				this.transform(this.getTransformData(0));
+				res()})	
+			}
+
+				console.log("out");
 			res();
 		})
+	}
+	getTransformData(aNum){
+		return teresa.getTransformData(aNum);
+	}
+	static getTransformData(aNum){
+		let tData=[
+			teresa.getCharaData(),
+			{NAME:"テレサ",
+							HP:100,
+							MP:20,
+							ATK:28,
+							DEF:20,
+							SPD:22,
+							MOV:2,
+							TYPE:"club",
+							IMAGE:1098020001,
+							DECK:teresa.getDeckTransform0(),
+							CHANGE:0
+			},
+		]
+		if(aNum==undefined) return tData;
+		else return tData[aNum];
+	}
+	static getDeckTransform0(){
+		return [
+			{NUMBER:101,
+				TEXT:"隣のマスに威力５のダメージ",
+				RANGE:[["distance",1]],
+				POWER:5,
+				DELAY:0,
+				MAGIC:0,
+				SUPPORT_Be_Myself:[],
+				SUPPORT_Af_Myself:[],
+				SUPPORT_Be_Enemy:[],
+				SUPPORT_Af_Enemy:[],
+				SUPPORT_Otherwise:[],
+				M_ATTACK:0,
+				F_ATTACK:false,
+		    E_ATTACK:true,
+				ANIMATION:[4]
+			},
+			{NUMBER:102,
+				TEXT:"２マス隣に威力４のダメージ",
+				RANGE:[["distance",2]],
+				POWER:4,
+				DELAY:0,
+				MAGIC:0,
+				SUPPORT_Be_Myself:[],
+				SUPPORT_Af_Myself:[],
+				SUPPORT_Be_Enemy:[],
+				SUPPORT_Af_Enemy:[],
+				SUPPORT_Otherwise:[],
+				M_ATTACK:0,
+				F_ATTACK:false,
+		    E_ATTACK:true,
+				ANIMATION:[0]
+			},
+			{NUMBER:103,
+				TEXT:"３マス隣に威力４のダメージ",
+				RANGE:[["distance",3]],
+				POWER:4,
+				DELAY:0,
+				MAGIC:0,
+				SUPPORT_Be_Myself:[],
+				SUPPORT_Af_Myself:[],
+				SUPPORT_Be_Enemy:[],
+				SUPPORT_Af_Enemy:[],
+				SUPPORT_Otherwise:[],
+				M_ATTACK:0,
+				F_ATTACK:false,
+		    E_ATTACK:true,
+				ANIMATION:[0]
+			},
+			{NUMBER:104,
+				TEXT:"縦一列に威力３のダメージ",
+				RANGE:[["vertical",1]],
+				POWER:3,
+				DELAY:0,
+				MAGIC:0,
+				SUPPORT_Be_Myself:[],
+				SUPPORT_Af_Myself:[],
+				SUPPORT_Be_Enemy:[],
+				SUPPORT_Af_Enemy:[],
+				SUPPORT_Otherwise:[],
+				M_ATTACK:0,
+				F_ATTACK:false,
+		    E_ATTACK:true,
+				ANIMATION:[0]
+			},
+			{NUMBER:105,
+				TEXT:"横一列に威力３ダメージ",
+				RANGE:[["horizontal",1]],
+				POWER:3,
+				DELAY:0,
+				MAGIC:0,
+				SUPPORT_Be_Myself:[],
+				SUPPORT_Af_Myself:[],
+				SUPPORT_Be_Enemy:[],
+				SUPPORT_Af_Enemy:[],
+				SUPPORT_Otherwise:[],
+				M_ATTACK:0,
+				F_ATTACK:false,
+		    E_ATTACK:true,
+				ANIMATION:[0]
+			},
+			{NUMBER:106,
+				TEXT:"３列の十字に威力２ダメージ",
+				RANGE:[["vertical",2],["horizontal",2]],
+				POWER:2,
+				DELAY:0,
+				MAGIC:0,
+				SUPPORT_Be_Myself:[],
+				SUPPORT_Af_Myself:[],
+				SUPPORT_Be_Enemy:[],
+				SUPPORT_Af_Enemy:[],
+				SUPPORT_Otherwise:[],
+				M_ATTACK:0,
+				F_ATTACK:false,
+		    E_ATTACK:true,
+				ANIMATION:[0]
+			},
+			{NUMBER:7,
+				TEXT:"シャッフルしてもう一度行動",
+				RANGE:[],
+				POWER:0,
+				DELAY:0,
+				MAGIC:1,
+				SUPPORT_Be_Myself:[],
+				SUPPORT_Af_Myself:[{effect:"getTurn"}],
+				SUPPORT_Be_Enemy:[],
+				SUPPORT_Af_Enemy:[],
+				SUPPORT_Otherwise:[{effect:"shuffle"}],
+				M_ATTACK:0,
+				F_ATTACK:false,
+				E_ATTACK:true,
+				ANIMATION:[0]
+			},
+			{NUMBER:108,
+				TEXT:"相手全体に威力２のダメージ",
+				RANGE:[["enemy"]],
+				POWER:2,
+				DELAY:10,
+				MAGIC:0,
+				SUPPORT_Be_Myself:[],
+				SUPPORT_Af_Myself:[],
+				SUPPORT_Be_Enemy:[],
+				SUPPORT_Af_Enemy:[],
+				SUPPORT_Otherwise:[],
+				M_ATTACK:0,
+				F_ATTACK:false,
+		    E_ATTACK:true,
+				ANIMATION:[5]
+			},
+			{NUMBER:109,
+				TEXT:"周囲８マスに威力３のダメージ",
+				RANGE:[["square",3]],
+				POWER:3,
+				DELAY:0,
+				MAGIC:0,
+				SUPPORT_Be_Myself:[],
+				SUPPORT_Af_Myself:[],
+				SUPPORT_Be_Enemy:[],
+				SUPPORT_Af_Enemy:[],
+				SUPPORT_Otherwise:[],
+				M_ATTACK:0,
+				F_ATTACK:false,
+		    E_ATTACK:true,
+				ANIMATION:[3]
+			},
+			{NUMBER:110,
+				TEXT:"ランダムな８マスに味方が踏むと２０回復する地雷を設置",
+				RANGE:[],
+				POWER:0,
+				DELAY:0,
+				MAGIC:2,
+				SUPPORT_Be_Myself:[],
+				SUPPORT_Af_Myself:[{effect:"setTrap",trapEffect:"mine",value:-20,target:["ally"],remove:true,range:[["random",8]]}],
+				SUPPORT_Be_Enemy:[],
+				SUPPORT_Af_Enemy:[],
+				SUPPORT_Otherwise:[],
+				M_ATTACK:0,
+				F_ATTACK:true,
+		    E_ATTACK:true,
+				ANIMATION:[4,3]
+			},
+			{NUMBER:111,
+				TEXT:"味方全体に威力４の回復",
+				RANGE:[["ally"]],
+				POWER:-4,
+				DELAY:0,
+				MAGIC:6,
+				SUPPORT_Be_Myself:[],
+				SUPPORT_Af_Myself:[],
+				SUPPORT_Be_Enemy:[],
+				SUPPORT_Af_Enemy:[],
+				SUPPORT_Otherwise:[],
+				M_ATTACK:-4,
+				F_ATTACK:true,
+		    E_ATTACK:false,
+				ANIMATION:[7]
+			},
+			{NUMBER:112,
+				TEXT:"２マス隣と３マス隣に威力４のダメージ",
+				RANGE:[["distance",2],["distance",3]],
+				POWER:4,
+				DELAY:0,
+				MAGIC:0,
+				SUPPORT_Be_Myself:[],
+				SUPPORT_Af_Myself:[],
+				SUPPORT_Be_Enemy:[],
+				SUPPORT_Af_Enemy:[],
+				SUPPORT_Otherwise:[],
+				M_ATTACK:0,
+				F_ATTACK:false,
+		    E_ATTACK:true,
+				ANIMATION:[7]
+			},
+			{NUMBER:113,
+				TEXT:"味方に威力３の回復",
+				RANGE:[["ally"]],
+				POWER:-3,
+				DELAY:0,
+				MAGIC:3,
+				SUPPORT_Be_Myself:[],
+				SUPPORT_Af_Myself:[],
+				SUPPORT_Be_Enemy:[],
+				SUPPORT_Af_Enemy:[],
+				SUPPORT_Otherwise:[],
+				M_ATTACK:0,
+				F_ATTACK:true,
+		    E_ATTACK:false,
+				ANIMATION:[7]
+			},
+			{NUMBER:114,
+				TEXT:"味方全体に威力４の回復 その後シャッフル",
+				RANGE:[["enemy"]],
+				POWER:-4,
+				DELAY:0,
+				MAGIC:0,
+				SUPPORT_Be_Myself:[],
+				SUPPORT_Af_Myself:[],
+				SUPPORT_Be_Enemy:[],
+				SUPPORT_Af_Enemy:[],
+				SUPPORT_Otherwise:[{effect:"revers"},{effect:"shuffle"}],
+				M_ATTACK:-4,
+				F_ATTACK:true,
+		    E_ATTACK:false,
+				ANIMATION:[7]
+			},
+			{NUMBER:0,
+					TEXT:"スカ　何も起こらない",
+					RANGE:[],
+					POWER:0,
+					DELAY:0,
+					MAGIC:0,
+					SUPPORT_Be_Myself:[],
+					SUPPORT_Af_Myself:[],
+					SUPPORT_Be_Enemy:[],
+					SUPPORT_Af_Enemy:[],
+					SUPPORT_Otherwise:[{effect:"revers"}],
+					M_ATTACK:0,
+					F_ATTACK:false,
+			    E_ATTACK:true,
+					ANIMATION:[0]
+				},]
 	}
 }
